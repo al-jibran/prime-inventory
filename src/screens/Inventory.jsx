f57d21020413e285786d3e8a00dd5929dafeae26
Al Jibran <jibran.web@gmail.com>
2021-06-14 15:48:58 +0530

    Move ProductItem into its own component file ProductItem.jsx

--- src/screens/Inventory.jsx
+++ src/screens/Inventory.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Toolbar from '../components/Toolbar';
 import { View, FlatList, Text } from 'react-native';
 import styled from 'styled-components';
+import ProductItem from '../components/ProductItem';
 
 const SearchStyle = styled.TextInput`
     margin-top: 30px;
@@ -11,24 +12,6 @@ const SearchStyle = styled.TextInput`
     border-radius: 30px;
     padding: 5px;
 `;
-
-const ProductItem = styled.View`
-    width: 99%;
-    height: 75px;
-    margin: 20px auto;
-    padding-left: 20px;
-    shadow-color: #ddd;
-    shadow-offset: 1px 1px;
-    shadow-opacity: 1;
-    shadow-radius: 3px;
-    elevation: 5;
-    background-color: #fff;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-`;
-
 
 const products = [{
     id: 1,
@@ -47,19 +30,7 @@ const products = [{
     stock: 10,
 }];
 
-const renderItem = ({ item }) => {
-    return (
-        <ProductItem>
-            <View style={{flexGrow: 3 }}>
-                <Text>{item.product}</Text>
-                <Text>{item.brand}</Text>
-            </View>
-            <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fbca1d', alignSelf: 'stretch', }}>
-                <Text>{item.stock}</Text>
-            </View>
-        </ProductItem>
-    );
-};
+const renderItem = ({ item }) => <ProductItem item={item} />;
 
 const Inventory = () => {
     return (
