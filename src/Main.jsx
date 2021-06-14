528537c06338b81be4b1a9ddcab54971b54250ad
Al Jibran <jibran.web@gmail.com>
2021-06-14 17:05:16 +0530

    Fix: Toolbar top margin touching the status bar

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,7 @@ import Constants from 'expo-constants';
 import Inventory from './screens/Inventory';
 
 const Container = styled.View`
-    margin-top: ${Constants.statusBarHeight}px;
+    margin-top: ${Constants.statusBarHeight + 15}px;
     margin-left: 40px;
     margin-right: 40px;
 `;
