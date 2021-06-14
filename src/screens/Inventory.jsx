07175075f0d7a77d0c67f78088739ab3a7217084
Al Jibran <jibran.web@gmail.com>
2021-06-14 15:40:30 +0530

    Use Flexbox to align Product List Items

--- src/screens/Inventory.jsx
+++ src/screens/Inventory.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Toolbar from '../components/Toolbar';
-import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import styled from 'styled-components';
 
 const SearchStyle = styled.TextInput`
-    margin: 20px auto;
+    margin-top: 30px;
+    margin-bottom: 10px;
     border: 1px solid black;
     width: 100%;
     border-radius: 30px;
@@ -13,14 +14,19 @@ const SearchStyle = styled.TextInput`
 
 const ProductItem = styled.View`
     width: 99%;
+    height: 75px;
     margin: 20px auto;
-    padding-left: 10px;
+    padding-left: 20px;
     shadow-color: #ddd;
     shadow-offset: 1px 1px;
     shadow-opacity: 1;
     shadow-radius: 3px;
     elevation: 5;
     background-color: #fff;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 `;
 
 
@@ -44,9 +50,13 @@ const products = [{
 const renderItem = ({ item }) => {
     return (
         <ProductItem>
-            <Text>{item.product}</Text>
-            <Text>{item.brand}</Text>
-            <Text>{item.stock}</Text>
+            <View style={{flexGrow: 3 }}>
+                <Text>{item.product}</Text>
+                <Text>{item.brand}</Text>
+            </View>
+            <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fbca1d', alignSelf: 'stretch', }}>
+                <Text>{item.stock}</Text>
+            </View>
         </ProductItem>
     );
 };
