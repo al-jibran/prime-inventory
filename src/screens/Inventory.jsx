e0a8e737dbb410a26d4c2009d505087347022b63
Al Jibran <jibran.web@gmail.com>
2021-06-14 14:34:40 +0530

    Add FlatList with Dummy Data

--- src/screens/Inventory.jsx
+++ src/screens/Inventory.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Toolbar from '../components/Toolbar';
-import { TextInput, View } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import styled from 'styled-components';
 
 const SearchStyle = styled.TextInput`
@@ -11,11 +11,42 @@ const SearchStyle = styled.TextInput`
     padding: 5px;
 `;
 
+const products = [{
+    id: 1,
+    product: 'Product 1',
+    brand: 'Brand A',
+    stock: 5,
+}, {
+    id: 2,
+    product: 'Product 2',
+    brand: 'Brand B',
+    stock: 1,
+}, {
+    id: 3,
+    product: 'Product 3',
+    brand: 'Brand C',
+    stock: 10,
+}];
+
+const renderItem = ({ item }) => {
+    return (
+        <View>
+            <Text>{item.product}</Text>
+            <Text>{item.brand}</Text>
+            <Text>{item.stock.toString()}</Text>
+        </View>
+    );
+};
+
 const Inventory = () => {
     return (
         <View>
             <Toolbar />
-            <SearchStyle placeholder="Search" clearButtonMode='while-editing'/>
+            <SearchStyle placeholder="Search" clearButtonMode='while-editing' />
+            <FlatList
+            data={products} 
+            keyExtractor={item =>  item.id}
+            renderItem={renderItem} />
         </View>
     );
 };
