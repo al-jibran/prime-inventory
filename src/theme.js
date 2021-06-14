1dbba6307b6caf1a1aa2fd5f934600da127d0733
Al Jibran <jibran.web@gmail.com>
2021-06-14 17:22:13 +0530

    Add theme.js with Theme for the app

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+const Theme = {
+    fontSize: {
+        heading: 18,
+        subheading: 16,
+        body: 14,
+        subtex: 11
+    },
+
+    fontWeight: {
+        normal: 400,
+        bold: 600
+    },
+
+    color: {
+        textPrimary: "#000000",
+        textSecondary: "#ABABAB",
+        primary: "#FB1D1D",
+        success: "#1DFB83",
+        warning: "#FBCA1D"
+    },
+};
+
+export default Theme;
