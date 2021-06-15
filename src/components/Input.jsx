8ab236805ddd291589c132a1c1fe6dcef0de5950
Al Jibran <jibran.web@gmail.com>
2021-06-15 12:07:21 +0530

    Add Input.jsx to handle input

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, TextField } from 'react-native';
+
+const Input = () => {
+    return (
+        <View>
+            <TextField />
+        </View>
+    );
+};
+
+export default Input;
