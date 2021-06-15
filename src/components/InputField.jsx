11d3a12d21bd81fa6b6a56518612f405bb2e61a9
Al Jibran <jibran.web@gmail.com>
2021-06-15 15:37:16 +0530

    Separate TextInput Type and Number Input Type

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Pressable } from 'react-native';
 import styled from 'styled-components';
 import { Text } from './Text';
 
@@ -7,14 +7,42 @@ import { Text } from './Text';
 const Field = styled.View`
     display: flex;
     flex-direction: row;
-    margin: 10px auto;
+    margin-top: 10px;
+    margin-bottom: 10px;
 `;
 
-const InputField = ({label, type, ...props}) => {
+const TextInputStyle = styled.TextInput`
+  border-bottom-color: black;
+  border-bottom-width: 1px;
+  flex-grow: 1; 
+  margin-left: 5px;
+`;
+
+const NumberInputStyle = styled(TextInputStyle)`
+    width: 40px;
+    flex-grow: 0;
+    text-align: center;
+`;
+
+const InputField = ({ label, type, ...props }) => {
+    const InputArea = ({ type, props }) => {
+        switch (type) {
+            case 'text': return <TextInputStyle {...props} />;
+            case 'number': return (
+                <View style={({flexDirection: 'row', marginLeft: 15})}>
+                    <Pressable onPress={null}><Text>+</Text></Pressable>
+                    <NumberInputStyle {...props} keyboardType={'numeric'} />
+                    <Pressable onPress={null}><Text>-</Text></Pressable>
+                </View>
+            );
+        }
+        return null;
+    };
+
     return (
         <Field>
             <Text>{label}</Text>
-            <TextInput {...props} style={({borderBottomColor: 'black', borderBottomWidth: 1, flexGrow: 1, marginLeft: 5})}/>
+            <InputArea type={type} props={props} />
         </Field>
     );
 };
