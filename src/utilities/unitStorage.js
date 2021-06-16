11d104e6af6a3d34b979b0e8f56e09c280b757a8
Al Jibran <jibran.web@gmail.com>
2021-06-16 17:22:38 +0530

    unitStorage class for abstracting the storage functionality of Async Storage for units

---
+++ src/utilities/unitStorage.js
@@ -0,0 +1,34 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+class UnitStorage {
+    constructor(namespace = 'unit') {
+        this.namespace = namespace;
+    }
+
+    async getUnitValue(unit) {
+        try {
+            const unitValue = await AsyncStorage.getItem(`${this.namespace}:${unit}`);
+            return unitValue ? unitValue : null;
+        } catch (error) {
+            console.log("Failed to retreive value:", error.message);
+        }
+    }
+
+    async setUnitValue(unit, value) {
+        try {
+            await AsyncStorage.setItem(`${this.namespace}:${unit}`, value);
+        } catch (error) {
+            console.log("Failed to set value:", error.message);
+        }
+    }
+
+    async removeUnitValue(unit) {
+        try {
+        await AsyncStorage.removeItem(`${this.namespace}:${unit}`);
+        } catch (error) {
+            console.log("Failed to remove value:", error.message);
+        }
+    }
+}
+
+export default UnitStorage;
