2eb547b31a5d2b2604ffbd89534cd96fea907336
Al Jibran <jibran.web@gmail.com>
2021-06-16 12:31:54 +0530

    Working + and - for increment and decrement

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useField } from 'formik';
-import { View } from 'react-native';
+import { View, Pressable } from 'react-native';
 import styled from 'styled-components';
 import { Text, SubText } from './Text';
 
 // Check styled documentation
-const Field = styled.View`
+const FieldStyle = styled.View`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -13,16 +13,22 @@ const Field = styled.View`
     margin-bottom: 10px;
 `;
 
-const TextInputStyle = styled.TextInput`
+const TextFieldStyle = styled.TextInput`
   border-bottom-color: black;
   border-bottom-width: 1px;
   margin-left: 5px;
   width: 50%;
 `;
 
-const NumberInputStyle = styled(TextInputStyle)`
+const NumberFieldStyle = styled(TextFieldStyle)`
   text-align: center; 
-  width: 10%;
+  width: 30%;
+`;
+
+const NumberInputStyle = styled.View`
+  flex-direction: row;
+  align-self: flex-end;
+  justify-content: flex-end;
 `;
 
 export const TextInput = ({ label, name, ...props }) => {
@@ -30,37 +36,50 @@ export const TextInput = ({ label, name, ...props }) => {
   const showError = fieldMeta.touched && fieldMeta.error;
   return (
     <View>
-      <Field>
+      <FieldStyle>
         <Text>{label}</Text>
-        <TextInputStyle
+        <TextFieldStyle
           value={field.value}
           onChangeText={text => fieldHelpers.setValue(text)}
           onBlur={() => fieldHelpers.setTouched(true)}
           keyboardType="numeric"
           {...props} />
 
-      </Field>
+      </FieldStyle>
       {showError && <SubText color="red">{fieldMeta.error}</SubText>}
     </View>
   );
 };
 
+
 export const NumberInput = ({ label, name, ...props }) => {
   const [field, fieldMeta, fieldHelpers] = useField(name);
   const showError = fieldMeta.touched && fieldMeta.error;
+
+  const increment = () => fieldHelpers.setValue((parseInt(field.value) + 1).toString());
+  const decrement = () => fieldHelpers.setValue((parseInt(field.value) - 1).toString());
+
   return (
     <View>
-      <Field>
+      <FieldStyle>
         <Text>{label}</Text>
-        <NumberInputStyle
-          value={field.value}
-          onChangeText={text => fieldHelpers.setValue(text)}
-          onBlur={() => fieldHelpers.setTouched(true)}
-          keyboardType="numeric"
-          clearTextOnFocus
-          {...props} />
+        <NumberInputStyle>
+          <Pressable onPress={increment}>
+            <Text>+</Text>
+          </Pressable>
+          <NumberFieldStyle
+            value={field.value}
+            onChangeText={text => fieldHelpers.setValue(text)}
+            onBlur={() => fieldHelpers.setTouched(true)}
+            keyboardType="numeric"
+            clearTextOnFocus
+            {...props} />
+            <Pressable onPress={decrement}>
+              <Text>-</Text>
+            </Pressable>
+        </NumberInputStyle>
 
-      </Field>
+      </FieldStyle>
       {showError && <SubText color="red">{fieldMeta.error}</SubText>}
     </View>
   );
