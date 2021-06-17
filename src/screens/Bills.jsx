16a629537e69cd42d7da073c5ae89a761959705e
Al Jibran <jibran.web@gmail.com>
2021-06-17 21:18:29 +0530

    Add toolbar to Bills screen

--- src/screens/Bills.jsx
+++ src/screens/Bills.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Text} from '../components/Text';
 import { View } from 'react-native';
-
+import { Container } from '../styles/common';
+import Toolbar from '../components/Toolbar';
 
 const Bills = () => {
   return (
+    <Container padLeft={20} padRight={20}>
+      <Toolbar items={ToolbarItems} justifyItems="center"/>
+    </Container>
+  );
+};
+
+const ToolbarItems = () => {
+  return (
     <View>
-      <Text>Hello, Bills</Text>
+      <Text>Bills</Text>
     </View>
   );
 };
