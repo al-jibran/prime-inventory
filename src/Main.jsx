d1318c7fa5f69b249310d0e410f034a125d5988d
Al Jibran <jibran.web@gmail.com>
2021-06-17 17:15:05 +0530

    Add Icons to Navigation Bar

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
-import Constants from 'expo-constants';
 import Inventory from './screens/Inventory';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddProduct from './components/AddProduct';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        // eslint-disable-next-line react/display-name
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Inventory') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Bills') {
+            iconName = `ios-list`;
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+
+      })} initialRouteName="Bills">
+        <Tab.Screen name="Bills" component={AddProduct} />
         <Tab.Screen name="Inventory" component={Inventory} />
-        <Tab.Screen name="Bills" component={AddProduct}/>
       </Tab.Navigator>
     </NavigationContainer>
 
