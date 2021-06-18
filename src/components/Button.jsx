0e582d5fd0b6117af16ea32ec28466b5289c191c
Al Jibran <jibran.web@gmail.com>
2021-06-18 11:45:09 +0530

    Create Custom Button Component

---
+++ src/components/Button.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled, { css } from 'styled-components/native';
+import Theme from '../theme';
+
+const ButtonContainer = styled.Pressable`
+  background-color: ${({ bgColor }) => {
+    if (bgColor) {
+      switch (bgColor) {
+        case 'primary': return Theme.color.primary;
+        case 'success': return Theme.color.success;
+        case 'warning': return Theme.color.warning;
+        default: return bgColor;
+      }
+    } else {
+      return Theme.color.textPrimary;
+    }
+  }};
+
+  ${({bgColor}) => bgColor === 'white' && css `border: 1px solid black;`}
+
+  padding-top: ${props => props.padTop || 5}px;
+  padding-bottom: ${props => props.padBottom || 5}px;
+  padding-left: ${props => props.padLeft || 5}px;
+  padding-right: ${props => props.padLeft || 5}px;
+  width: ${props => props.width || '33'}%
+  
+  ${({ rounded }) => rounded && css`border-radius: 10px`};
+`;
+
+
+const ButtonText = styled.Text`
+  text-align: center;
+  color: ${props => props.color || props.bgColor === 'white' ? Theme.color.textPrimary : 'white'};
+`;
+
+const Button = ({ text, onPress, ...props }) => {
+  return (
+    <ButtonContainer onPress={onPress} {...props}>
+      <ButtonText color={props.color} {...props}>{text}</ButtonText>
+    </ButtonContainer>
+  );
+};
+
+export default Button;
