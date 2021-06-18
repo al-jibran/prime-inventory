08ed26d50d327f4e7d89bd750b7fca501fab9e4e
Al Jibran <jibran.web@gmail.com>
2021-06-18 17:58:35 +0530

    Fix Inventory items not showing up due to css styles

--- src/screens/Inventory.jsx
+++ src/screens/Inventory.jsx
@@ -14,13 +14,26 @@ import Toolbar from '../components/Toolbar';
 //Styles
 import { Container } from '../styles/common';
 
+
+const renderItem = (item, products, dispatch) => {
+  // Opens a delete alert with Alert.alert() and displays a title, message and buttons for the action.
+  const deleteTitle = `Delete ${item.product}?`;
+  const deleteMessage = `The product ${item.product} from ${item.brand} will be deleted permanently.\n\nDo you want to continue?`;
+  const buttons = [{ text: "Cancel", onPress: null, style: "cancel" },
+  { text: "Yes", onPress: () => dispatch(deleteProduct(item.id)) }];
+
+  return (<Pressable onLongPress={() => Alert.alert(deleteTitle, deleteMessage, buttons)}>
+    <ProductItem item={item} products={products} />
+  </Pressable>);
+};
+
 const Inventory = () => {
   const [visible, setVisiblity] = useState(false);
 
   return (
     <Container padLeft={20} padRight={20}>
       <Toolbar items={() => <ToolbarItems visible={visible} toggleModal={setVisiblity} />} />
-      <ProductsList renderItem={renderItem}/>
+      <ProductsList />
       <Modal visible={visible}>
         <AddProduct setVisible={setVisiblity} />
       </Modal>
@@ -28,23 +41,7 @@ const Inventory = () => {
   );
 };
 
-const ToolbarItems = ({ visible, toggleModal }) => {
-  const onPressAdd = () => {
-    toggleModal(!visible);
-  };
-
-  return (
-    <>
-      <Text>Filter</Text>
-      <Text>Inventory</Text>
-      <Pressable onPress={onPressAdd}>
-        <Text>Add</Text>
-      </Pressable>
-    </>
-  );
-};
-
-const ProductsList = ({renderItem}) => {
+const ProductsList = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [products, dispatch] = useStore();
 
@@ -66,22 +63,24 @@ const ProductsList = ({renderItem}) => {
       renderItem={({ item }) => renderItem(item, products, dispatch)}
       showsHorizontalScrollIndicator={false}
       showsVerticalScrollIndicator={false}
-      contentContainerStyle={({display: 'none'})}
-      ListHeaderComponentStyle={({})}
     />
   );
 };
 
-const renderItem = (item, products, dispatch) => {
-  // Opens a delete alert with Alert.alert() and displays a title, message and buttons for the action.
-  const deleteTitle = `Delete ${item.product}?`;
-  const deleteMessage = `The product ${item.product} from ${item.brand} will be deleted permanently.\n\nDo you want to continue?`;
-  const buttons = [{ text: "Cancel", onPress: null, style: "cancel" },
-  { text: "Yes", onPress: () => dispatch(deleteProduct(item.id)) }];
+const ToolbarItems = ({ visible, toggleModal }) => {
+  const onPressAdd = () => {
+    toggleModal(!visible);
+  };
 
-  return (<Pressable onLongPress={() => Alert.alert(deleteTitle, deleteMessage, buttons)}>
-    <ProductItem item={item} products={products} />
-  </Pressable>);
+  return (
+    <>
+      <Text>Filter</Text>
+      <Text>Inventory</Text>
+      <Pressable onPress={onPressAdd}>
+        <Text>Add</Text>
+      </Pressable>
+    </>
+  );
 };
 
 
