6031d42641d9f196f8470966722e8079e760f322
Al Jibran <jibran.web@gmail.com>
2021-06-21 14:19:09 +0530

    Change EditProduct action to take id and changes to be made as parameter. Calculate the change in stock within reducer rather than receiving it from client

--- src/productReducer.js
+++ src/productReducer.js
@@ -1,70 +1,78 @@
 export const initialState = [{
-    id: 1,
-    name: 'Product 1',
-    brand: 'Brand A',
-    stock: 5,
+  id: 1,
+  name: 'Product 1',
+  brand: 'Brand A',
+  stock: 5,
 }, {
-    id: 2,
-    name: 'Product 2',
-    brand: 'Brand B',
-    stock: 1,
+  id: 2,
+  name: 'Product 2',
+  brand: 'Brand B',
+  stock: 1,
 }, {
-    id: 3,
-    name: 'Product 3',
-    brand: 'Brand C',
-    stock: 13,
+  id: 3,
+  name: 'Product 3',
+  brand: 'Brand C',
+  stock: 13,
 }, {
-    id: 4,
-    name: 'Product 4',
-    brand: 'Brand A',
-    stock: 2,
+  id: 4,
+  name: 'Product 4',
+  brand: 'Brand A',
+  stock: 2,
 }, {
-    id: 5,
-    name: 'Product 5',
-    brand: 'Brand B',
-    stock: 10,
+  id: 5,
+  name: 'Product 5',
+  brand: 'Brand B',
+  stock: 10,
 }];
 
 export const reducer = (state, action) => {
-    switch(action.type) {
-        case 'GET_PRODUCTS': return state;
-        case 'ADD_PRODUCT': return state.concat(action.payload);
-        case 'EDIT_PRODUCT': {
-            const product = action.payload;
-            return state.map(p => p.id !== product.id ? p : product);
-        }
-        case 'DELETE_PRODUCT': {
-            return state.filter(product => product.id !== action.payload.id);
-        }
-        default: return state;
+  switch (action.type) {
+    case 'GET_PRODUCTS': return state;
+    case 'ADD_PRODUCT': return state.concat(action.payload);
+    case 'EDIT_PRODUCT': {
+      const id = action.payload.id;
+      let changed = action.payload.change;
+      const { stock } = Object.prototype.hasOwnProperty.call(changed, 'stock') ? changed : 0;
+      return state.map(product => {
+        changed = { ...changed, stock: stock + product.stock };
+        return product.id !== id ? product : { ...product, ...changed };
+      });
     }
+    case 'DELETE_PRODUCT': {
+      return state.filter(product => product.id !== action.payload.id);
+    }
+    default: return state;
+  }
 };
 
 export const getProducts = () => {
-    return {
-        type: 'GET_PRODUCTS',
-    };
+  return {
+    type: 'GET_PRODUCTS',
+  };
 };
 
 export const addProduct = (product) => {
-    return {
-        type: 'ADD_PRODUCT',
-        payload: product,
-    };
+  return {
+    type: 'ADD_PRODUCT',
+    payload: product,
+  };
 };
 
-export const editProduct = (product) => {
-    return {
-        type: 'EDIT_PRODUCT',
-        payload: product,
-    };
-} ;
+export const editProduct = (id, change) => {
+  return {
+    type: 'EDIT_PRODUCT',
+    payload: {
+      id,
+      change
+    },
+  };
+};
 
 export const deleteProduct = (id) => {
-    return {
-        type: 'DELETE_PRODUCT',
-        payload: {
-            id
-        }
-    };
+  return {
+    type: 'DELETE_PRODUCT',
+    payload: {
+      id
+    }
+  };
 };
