6438e160058a79959fea7a8a6e5b51bd2b2fca02
Al Jibran <jibran.web@gmail.com>
2021-07-17 17:14:04 +0530

    remove unnecessary commits

--- src/screens/Product.jsx
+++ src/screens/Product.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { gql, useApolloClient } from "@apollo/client";
-import { Text, Heading } from "../components/Text";
-import { GET_PRODUCT } from "../graphql/queries";
+import { Heading } from "../components/Text";
 
 const Product = ({ route }) => {
   const client = useApolloClient();
   const { id } = route.params;
 
-  const { name, brand, stock } = client.readFragment({
+  const { name } = client.readFragment({
     id: `Product:${id}`,
     fragment: gql`
       fragment P1 on Product {
