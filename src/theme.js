14c72fdca8e987de87d43af5dc73ff76e316ced9
Al Jibran <jibran.web@gmail.com>
2021-07-21 16:04:44 +0530

    add light font weight to theme

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,25 @@
 const Theme = {
-    fontSize: {
-        heading: 18,
-        subheading: 16,
-        body: 14,
-        subtext: 11
-    },
+  fontSize: {
+    heading: 18,
+    subheading: 16,
+    body: 14,
+    subtext: 11,
+  },
 
-    fontWeight: {
-        normal: 400,
-        bold: 600
-    },
+  fontWeight: {
+    light: 200,
+    normal: 400,
+    bold: 600,
+  },
 
-    color: {
-        textPrimary: "#000000",
-        textSecondary: "#ABABAB",
-        primary: "#1EB8E1",
-        danger: "#FB1D1D",
-        success: "#2EDF66",
-        warning: "#FBCA1D"
-    },
+  color: {
+    textPrimary: "#000000",
+    textSecondary: "#ABABAB",
+    primary: "#1EB8E1",
+    danger: "#FB1D1D",
+    success: "#2EDF66",
+    warning: "#FBCA1D",
+  },
 };
 
 export default Theme;
